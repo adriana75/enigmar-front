64de9b4af08e95984d5a84e225da8cd604ad1c3c
Danny Gómez <styven.gomez@udea.edu.co>
2021-12-10 13:18:08 -0500

    Prueba tabla incripciones

--- enigmar-front/src/App.js
+++ enigmar-front/src/App.js
@@ -1,34 +1,40 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import IndexU from './pages/users/IndexUsers' 
-import IndexP from './pages/projects/IndexProjects'
-import IndexE from './pages/enrollments/IndexEnrollments'
-import IndexA from './pages/advances/IndexAdvances'
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import UpdateUser from './pages/users/UpdateUser';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import IndexU from "./pages/users/IndexUsers";
+import IndexP from "./pages/projects/IndexProjects";
+import IndexE from "./pages/enrollments/IndexEnrollments";
+import IndexA from "./pages/advances/IndexAdvances";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import UpdateUser from "./pages/users/UpdateUser";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql"
-})
+  uri: "http://localhost:4000/graphql",
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link:  httpLink,
-})
+  link: httpLink,
+});
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Routes>
-          <Route path='/usuarios' element={<IndexU />}/>
-          <Route path='/proyectos' element={<IndexP />}/>
-          <Route path='/inscripciones' element={<IndexE />}/>
-          <Route path='/avances' element={<IndexA />}/>
-          <Route path='/usuarios/editar/:_id' element={<UpdateUser />} />
+          <Route path="/usuarios" element={<IndexU />} />
+          <Route path="/proyectos" element={<IndexP />} />
+          <Route path="/inscripciones" element={<IndexE />} />
+          <Route path="/avances" element={<IndexA />} />
+          <Route path="/usuarios/editar/:_id" element={<UpdateUser />} />
         </Routes>
       </BrowserRouter>
-    </ApolloProvider> 
+    </ApolloProvider>
   );
 }
 
--- enigmar-front/src/pages/enrollments/IndexEnrollments.jsx
+++ enigmar-front/src/pages/enrollments/IndexEnrollments.jsx
@@ -1,50 +1,50 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client'
-import { GET_INSCRIPCIONES } from '../../graphql/enrollments/queries';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_INSCRIPCIONES } from "../../graphql/enrollments/queries";
+import { Table, Container, Button, Image, Row, Figure } from "react-bootstrap";
 
 const IndexEnrollments = () => {
+  const { data, error, loading } = useQuery(GET_INSCRIPCIONES);
+  useEffect(() => {
+    console.log("data servidor", data);
+  }, [data]);
 
-    const { data, error, loading } = useQuery(GET_INSCRIPCIONES);
-    useEffect(() => {
-        console.log('data servidor', data);
-    }, [data]);
+  return (
+    <div>
+      Datos Inscripciones:
+      <Table striped bordered hover fluid>
+        <thead>
+          <tr>
+            <th>Nombre del Proyecto</th>
+            <th>Nombre del Usuarios</th>
+            <th>Estado</th>
+            <th>Fecha de Inscripción</th>
+            <th>Fecha Final</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data && data.Enrollments ? (
+            <>
+              {data.Enrollments.map((e) => {
+                return (
+                  <tr key={e._id}>
+                    <td>{e.project_id.name}</td>
+                    <td>{e.user_id.name}</td>
+                    <td>{e.status}</td>
+                    <td>{e.enrollmentDate}</td>
+                    <td>{e.egressDate}</td>
+                  </tr>
+                );
+              })}
+            </>
+          ) : (
+            <div>No autorizado</div>
+          )}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
 
-    return (
-        <div>
-        Datos Inscripciones:
-        <table>
-          <thead>
-            <tr>
-              <th>Nombre del Proyecto</th>
-              <th>Nombre del Usuarios</th>
-              <th>Estado</th>
-              <th>Fecha de Inscripción</th>
-              <th>Fecha Final</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data && data.Enrollments ? (
-              <>
-                {data.Enrollments.map((e) => {
-                  return (
-                    <tr key={e._id}>
-                      <td>{e.project_id.name}</td>
-                      <td>{e.user_id.name}</td>
-                      <td>{e.status}</td>
-                      <td>{e.enrollmentDate}</td>
-                      <td>{e.egressDate}</td>  
-                    </tr>
-                  );
-                })}
-              </>
-            ) : (
-              <div>No autorizado</div>
-            )}
-          </tbody>
-        </table>
-      </div>
-    )
-}
-
-export default IndexEnrollments
+export default IndexEnrollments;
