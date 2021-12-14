fb1da9456b7f3b4d104df69583bf2d1219dcadb9
adriana75 <tecnologianormal@hotmail.com>
2021-12-14 18:09:52 -0500

    Mejorando apariencia

--- enigmar-front/src/pages/Auth/Home.jsx
+++ enigmar-front/src/pages/Auth/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
                 <form>
                     <div>
                         <h1 className='text-center'>SISTEMA DE GESTIÓN DE PROYECTOS DE INVESTIGACIÓN</h1><hr/><br/><br/>
+                        <h2 className='text-center'>INGRESAR</h2><br/><br/>
                         <Input name='email' label='Correo electrónico: ' required={true} type='text' /><br/>
                         <Input name='password' label='Contraseña: ' required={true} type='text' /><br/>
                         <Link to={"/modulos/"} className='font-bold text-lg py-3 px-6  rounded-xl hover:bg-indigo-500 shadow-md my-2 disabled:opacity-50 disabled:bg-gray-700 btn btn-secondary mt-3 ms-3 mb-3'>Iniciar Sesión</Link>
