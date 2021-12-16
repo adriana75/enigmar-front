01aa74a6e879d5090c7e88373d6960b0237c0ff3
adriana75 <tecnologianormal@hotmail.com>
2021-12-16 10:51:55 -0500

    Mejorando login

--- src/pages/Auth/Home.jsx
+++ src/pages/Auth/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
                     <div>
                         <h1 className='text-center'>SISTEMA DE GESTIÓN PARA PROYECTOS DE INVESTIGACIÓN</h1><hr/><br/><br/>
                         <h2 className='text-center'>INGRESAR</h2><br/><br/>
-                        <Input name='email' label='Correo electrónico: ' required={true} type='text' /><br/>
-                        <Input name='password' label='Contraseña: ' required={true} type='text' /><br/>
+                        <p>Correo Electrónico:</p><input name='email' label='Correo electrónico: ' required={true} type='text' value="admin@enigma.com"/><br/><br/>
+                        <p>Contraseña:</p><input name='password' label='Contraseña: ' required={true} type='password' value="admin"/><br/><br/>
                         <Link to={"/modulos/"} className='font-bold text-lg py-3 px-6  rounded-xl hover:bg-indigo-500 shadow-md my-2 disabled:opacity-50 disabled:bg-gray-700 btn btn-secondary mt-3 ms-3 mb-3'>Iniciar Sesión</Link>
                         <Link to={"/usuarios/crear"} className='font-bold text-lg py-3 px-6  rounded-xl hover:bg-indigo-500 shadow-md my-2 disabled:opacity-50 disabled:bg-gray-700 btn btn-secondary mt-3 ms-3 mb-3'>Registrarse</Link>
                     </div>
